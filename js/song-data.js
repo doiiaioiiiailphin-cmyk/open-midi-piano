106f99f4d9784c0cd5c15fd7f7c3030379c54748
doiiaioiiiailphin-cmyk <doiiaioiiiailphin@gmail.com>
2026-04-27 13:10:58 +0800

    fix: replace incorrect MIDI files with correct pieces (Chopin Nocturne Op.9 No.2, Schubert Moments Musicaux Op.94 No.3, Grieg Lyric Pieces Op.43 No.1)

--- js/song-data.js
+++ js/song-data.js
@@ -1,6 +1,6 @@
 // 所有曲目均为公共领域（Public Domain）古典音乐
 // 来源：https://github.com/TF2-Engineer/classical-midis
-// 注：canon.mid 和 bumblebee.mid 来自其他公共领域 MIDI 资源，作品本身为公共领域
+// 注：canon.mid、bumblebee.mid 以及肖邦/舒伯特/格里格三首来自 kunstderfuge.com，作品本身为公共领域
 
 export const SONGS = [
   {
@@ -32,16 +32,16 @@ export const SONGS = [
     type: 'midi', url: './assets/midi/mozart_sonata_545_1.mid', data: null
   },
   {
-    name: '前奏曲 No.2 Op.28', artist: '肖邦',
+    name: '降E大调夜曲 Op.9 No.2', artist: '肖邦',
     type: 'midi', url: './assets/midi/chopin_nocturne_op9_2.mid', data: null
   },
   {
-    name: '流浪者幻想曲 D.760 第二乐章', artist: '舒伯特',
-    type: 'midi', url: './assets/midi/schubert_moment_d760_2.mid', data: null
+    name: '音乐的瞬间 Op.94 No.3', artist: '舒伯特',
+    type: 'midi', url: './assets/midi/schubert_musical_moment.mid', data: null
   },
   {
-    name: '抒情小品 Op.42 No.2', artist: '格里格',
-    type: 'midi', url: './assets/midi/grieg_morning.mid', data: null
+    name: '抒情小品 "蝴蝶" Op.43 No.1', artist: '格里格',
+    type: 'midi', url: './assets/midi/grieg_lyric_pieces.mid', data: null
   },
   {
     name: '野蜂飞舞', artist: '里姆斯基-科萨科夫',
